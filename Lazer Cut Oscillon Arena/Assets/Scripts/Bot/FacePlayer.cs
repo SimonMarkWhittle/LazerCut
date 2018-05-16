9412676c53b7e79d0d1863c5ea7f9a266e274aed
SimonMarkWhittle <smw2ed@virginia.edu>
2018-05-15 21:24:20 -0400

    Stopped FacePlayer jitter

--- Lazer Cut Oscillon Arena/Assets/Scripts/Bot/FacePlayer.cs
+++ Lazer Cut Oscillon Arena/Assets/Scripts/Bot/FacePlayer.cs
@@ -6,13 +6,13 @@ public class FacePlayer : MonoBehaviour {
 
     GameObject player;
     public float maxDegrees = 0f;
-    float maxRad;
+    // float maxRad;
 
 	// Use this for initialization
 	void Start () {
         player = GameManager.Instance.player;
 
-        maxRad = (Mathf.PI / 180f) * maxDegrees;
+        // maxRad = (Mathf.PI / 180f) * maxDegrees;
 	}
 	
 	// Update is called once per frame
@@ -21,7 +21,7 @@ public class FacePlayer : MonoBehaviour {
 
         Vector3 direction = (player.transform.position - transform.position).normalized;
 
-        if (maxRad == 0f) {
+        if (maxDegrees == 0f) {
             transform.up = direction;
         }
         else {
@@ -29,7 +29,15 @@ public class FacePlayer : MonoBehaviour {
             Vector3 newLook = Vector3.RotateTowards(transform.up, direction, maxRad * Time.deltaTime, 0f);
             transform.up = new Vector3(0, 0, newLook.z);
             */
-            transform.Rotate(new Vector3(0, 0, (Vector2.SignedAngle(transform.up, direction))).normalized * 50 * Time.deltaTime); 
+
+
+            float angle = Vector2.SignedAngle(transform.up, direction);
+
+            if (Mathf.Abs(angle) < 1) { return; }
+
+            Vector3 rotate = new Vector3(0, 0, angle).normalized * maxDegrees * Time.deltaTime;
+            
+            transform.Rotate(rotate); 
             //Instead of 50 include some rotation speed var
             //Also, include some logic to prevent jittering
         }
